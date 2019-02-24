f62a95704259c36c83d177d2260426661b9fd3f6
Leo.fp <Leo.fp@kakaocorp.com>
2019-02-24 13:06:16 +0900

    Change dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import LRU from 'lru-cache';
+import { LRUMap } from 'lru_map'
 
-const memoize = (fn, { hasher = ((...args) => args.toString()), ...options } = {}) => {
-  const cache = new LRU({ ...options });
+const memoize = (fn, { hasher = ((...args) => args.toString()), max = 100 } = {}) => {
+  const cache = new LRUMap(max);
   return (...args) => {
     const key = hasher(...args);
     if (cache.has(key)) {
